304809d4665bce980753b7c1e569e9acdca4ec54
William Thomas <willizin2020@gmail.com>
2024-05-13 18:15:40 -0300

    atualizando exercicios

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+console.log(`A velociade do seu carro é ${vel}`)
+
+
+if( vel > 60){
+    console.log('Você ultrapassou o limite de velociade.MUTADO')
+
+}else{
+
+    console.log('Dirija sempre usando cinto de segurança')}
